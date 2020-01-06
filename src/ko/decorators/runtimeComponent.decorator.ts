623b1a2169cb6ae5b22b21a41dbd9e2cfdd06053
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-01-05 17:43:01 -0800

    tmp

--- src/ko/decorators/runtimeComponent.decorator.ts
+++ src/ko/decorators/runtimeComponent.decorator.ts
@@ -2,34 +2,25 @@ import * as ko from "knockout";
 
 export function RuntimeComponent(config: any): (target: Function) => void {
     return (target) => {
-        
-
         class RuntimeComponentProxy extends HTMLElement {
-            private onDispose: () => void;
-
             constructor() {
                 super();
             }
 
-            public connectedCallback(): void { 
+            public connectedCallback(): void {
                 const element = <HTMLElement>this;
 
                 ko.applyBindingsToNode(element, {
                     component: {
                         name: config.selector,
                         viewModel: target,
-                        params: element.getAttribute("params"),
-                        oncreate: (viewModelInstance) => {
-                            this.onDispose = viewModelInstance.dispose;
-                        }
+                        params: element.getAttribute("params")
                     }
                 }, null);
             }
 
             public disconnectedCallback(): void {
-                if (this.onDispose) {
-                    this.onDispose();
-                }
+                ko.cleanNode(this);
             }
         }
 

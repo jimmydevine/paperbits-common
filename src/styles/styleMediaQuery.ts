eaddddf229bca6486a2e51d3963879873acffd3d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-02-17 20:14:56 -0800

    Added media query generation from global JSS.

--- src/styles/styleMediaQuery.ts
+++ src/styles/styleMediaQuery.ts
@@ -3,14 +3,19 @@ import { Style } from "./styles";
 export class StyleMediaQuery {
     public minWidth: number;
     public styles: Style[];
+    public globalStyles: Style[];
 
     constructor(minWidth: number) {
         this.minWidth = minWidth;
         this.styles = [];
+        this.globalStyles = [];
     }
 
     public toJssString(): string {
-        const styles = this.styles.map(style => style.toJssString()).join();
-        return `"@media(min-width:${this.minWidth}px)":{${styles}}`;
+        const stylesJss = this.styles.map(style => style.toJssString()).join();
+        const globalStylesJss = this.globalStyles.map(style => style.toJssString()).join();
+        const jssString = `"@media(min-width:${this.minWidth}px)":{"@global":{${[globalStylesJss, stylesJss].filter(x => !!x).join(",")}}}`;
+
+        return jssString;
     }
 }
